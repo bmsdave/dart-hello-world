d96cbf17aacb5801c122e97b260b517f1e36a439
Vadim Gorbachev <bmsdave@gmail.com>
2016-10-04 01:30:24 +0300

    refactoring

---
+++ web/index.dart
@@ -0,0 +1,13 @@
+import 'package:polymer/polymer.dart';
+import 'package:paper_elements/paper_input_decorator.dart';
+import 'dart:html';
+
+
+export 'package:polymer/init.dart';
+
+@initMethod
+startup() {
+	
+	Polymer.onReady.then((_) {});
+	
+}
--- web/textarea.dart
+++ web/textarea.dart
@@ -10,21 +10,28 @@ class signin extends PolymerElement {
 
   @observable
   String text = '123';
+
   @observable
   String template = '321';
+
   var a1 = querySelector('#a1');
+
   var a2 = querySelector('#a2');
 
+
   check() async {
-//    var client = new BrowserClient();
-//    var url = "/api/check";
-//    client
-//        .post(url, body: {"text": text, "template": template}).then((response) {
-//      print("Response status: ${response.statusCode}");
-//      print("Response body: ${response.body}");
-//    });
+
+    //    var client = new BrowserClient();
+    //    var url = "/api/check";
+    //    client
+    //        .post(url, body: {"text": text, "template": template}).then((response) {
+    //      print("Response status: ${response.statusCode}");
+    //      print("Response body: ${response.body}");
+    //    });
+
     print("check?");
   }
+
 
   void changeTemplate(Event e) {
     template = (e.target as TextAreaElement).value;
@@ -33,13 +40,16 @@ class signin extends PolymerElement {
         .callMethod('update', [template]);
   }
 
+
   void changeText(Event e) {
     text = (e.target as TextAreaElement).value;
     a2.update();
   }
 
+
   void clear(Event e) {
     text = null;
     template = null;
   }
+  
 }
